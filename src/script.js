e99fdaa4dff6f0dfbd1fa309e49ff785eec78b8b
cjwit <chris.witulski@gmail.com>
2020-06-26 14:34:59 -0400

    change github reference to repo

--- src/script.js
+++ src/script.js
@@ -5,14 +5,14 @@ var visualizations = [
 		name: 'COVID-19 statistics',
 		img: 'images/covid.png',
 		description: "Interactive US State level representations of data related to the COVID-19 outbreak, including logorythmic total cases and deaths statistics and daily outbreaks, both total and scaled to state population",
-		github: "https://github.com/cjwit/covid",
+		repo: "https://github.com/cjwit/covid",
 		link: 'https://cjwit.github.io/covid/'
 	},
 	{
 		name: 'ContourViz: Melodic Contours in Malhun',
 		img: 'images/malhun.png',
 		description: "An interactive visualization of melodic transcriptions from a Moroccan musical genre of sung poetry. Used in conjunction with a presentation at the 2016 Analytical Approaches to World Music conference in New York City. Read a blog post about the Python package <a href = 'https://music21-mit.blogspot.com/2016/07/contourviz-creating-interactive-contour.html' target = '_blank'>here</a>.",
-		github: "https://github.com/cjwit/contourviz",
+		repo: "https://github.com/cjwit/contourviz",
 		link: 'http://cjwit.github.io/music21'
 	}
 ]
@@ -22,21 +22,21 @@ var websites = [
 		name: 'Worldmusictextbook.org',
 		img: 'images/wmt.png',
 		description: "An open and collaborative world music textbook.",
-		github: "https://github.com/worldmusic/worldmusic.github.io",
+		repo: "https://github.com/worldmusic/worldmusic.github.io",
 		link: 'https://worldmusictextbook.org'
 	},
 	{
 		name: 'Society for Arab Music Research',
 		img: 'images/samr.png',
 		description: "Jekyll based home page for the Arab music special interest group.",
-		github: "https://github.com/sem-samr/sem-samr.github.io",
+		repo: "https://github.com/sem-samr/sem-samr.github.io",
 		link: 'https://samr.club'
 	},
 	{
 		name: 'Building a (fretless, tackhead, block rim) banjo',
 		img: 'images/banjo.png',
 		description: "A step by step description of how I build banjos in a responsive and organized single page layout.",
-		github: "https://github.com/cjwit/banjo_build",
+		repo: "https://github.com/cjwit/banjo_build",
 		link: 'https://cjwit.github.io/banjo_build/'
 	}
 ]
@@ -46,21 +46,21 @@ var javascript = [
 		name: 'What?',
 		img: 'images/what.png',
 		description: "Javascript-based game that uses sounds and timers to clone the popular Simon©.",
-		github: "https://github.com/cjwit/what/tree/gh-pages",
+		repo: "https://github.com/cjwit/what/tree/gh-pages",
 		link: 'http://cjwit.github.io/what'
 	},
 	{
 		name: 'Tic Tac Toe',
 		img: 'images/ttt.png',
 		description: "Web-based tic tac toe game devleoped in Javascript.",
-		github: "https://github.com/cjwit/ttt/tree/gh-pages",
+		repo: "https://github.com/cjwit/ttt/tree/gh-pages",
 		link: 'http://cjwit.github.io/ttt'
 	},
 	{
 		name: 'Break Time',
 		img: 'images/break.png',
 		description: "Javascript-based pamadoro timer application.",
-		github: "https://github.com/cjwit/break/tree/gh-pages",
+		repo: "https://github.com/cjwit/break/tree/gh-pages",
 		link: 'http://cjwit.github.io/break'
 	}
 ]
@@ -71,49 +71,49 @@ var other_visualizations = [
 		name: 'Vote!',
 		img: 'images/vote.png',
 		description: "Voting app built in React/Flux using D3.js to represent user created polls.",
-		github: "https://github.com/cjwit/vote",
+		repo: "https://github.com/cjwit/vote",
 		link: 'https://cjwit-vote.herokuapp.com'
 	},
 	{
 		name: 'ContourViz: Melodic Contours in Malhun',
 		img: 'images/malhun.png',
 		description: "An interactive visualization of melodic transcriptions from a Moroccan musical genre of sung poetry. Used in conjunction with a presentation at the 2016 Analytical Approaches to World Music conference in New York City. Read a blog post about the Python package <a href = 'https://music21-mit.blogspot.com/2016/07/contourviz-creating-interactive-contour.html' target = '_blank'>here</a>.",
-		github: "https://github.com/cjwit/contourviz",
+		repo: "https://github.com/cjwit/contourviz",
 		link: 'http://cjwit.github.io/music21'
 	},
 	{
 		name: 'Meteorite Landings Worldwide',
 		img: 'images/meteors.png',
 		description: "A D3.js built global map displaying data for individual meteorite landings.",
-		github: "https://github.com/cjwit/meteors/tree/gh-pages",
+		repo: "https://github.com/cjwit/meteors/tree/gh-pages",
 		link: 'http://cjwit.github.io/meteors'
 	},
 	{
 		name: 'Camper News Postings on FreeCodeCamp.com',
 		img: 'images/posts.png',
 		description: "A force-directed interactive chart built in D3.js.",
-		github: "https://github.com/cjwit/campernews/tree/gh-pages",
+		repo: "https://github.com/cjwit/campernews/tree/gh-pages",
 		link: 'http://cjwit.github.io/campernews'
 	},
 	{
 		name: 'Global Temperatures since the 1750s',
 		img: 'images/temp.png',
 		description: "A heatmap type chart of global tempertures built in D3.js.",
-		github: "https://github.com/cjwit/globaltemp/tree/gh-pages",
+		repo: "https://github.com/cjwit/globaltemp/tree/gh-pages",
 		link: 'http://cjwit.github.io/globaltemp'
 	},
 	{
 		name: 'Doping Allegations in Cycling',
 		img: 'images/doping.png',
 		description: "An interactive scatterplot showing allegations of doping among top cyclists built in D3.js.",
-		github: "https://github.com/cjwit/doping/tree/gh-pages",
+		repo: "https://github.com/cjwit/doping/tree/gh-pages",
 		link: 'http://cjwit.github.io/doping'
 	},
 	{
 		name: 'Gross Domestic Product',
 		img: 'images/gdp.png',
 		description: "An interactive chart of US GDP growth built in D3.js.",
-		github: "https://github.com/cjwit/gdp/tree/gh-pages",
+		repo: "https://github.com/cjwit/gdp/tree/gh-pages",
 		link: 'http://cjwit.github.io/gdp'
 	}
 
@@ -122,49 +122,49 @@ var react = [
 		name: 'Arab Music Research',
 		img: 'images/amr.png',
 		description: "Full-stack React/Express/Node-based web application running through a MongoDB database that allows researchers to log in and share bios, events, and projects. Uses Facebook and Google authorization, deployed on Heroku. Currently under development.",
-		github: "https://github.com/cjwit/arabmusic/",
+		repo: "https://github.com/cjwit/arabmusic/",
 		link: 'https://whispering-sands-88923.herokuapp.com'
 	},
 	{
 		name: 'Getting Out',
 		img: 'images/out.png',
 		description: "React-Redux single page app that allows logged in users to share their evening's plans. The Node.js/MongoDB backend maintains a dated listing of users who have selected each hotspot, rendering them by username for others.",
-		github: "https://github.com/cjwit/gettingout",
+		repo: "https://github.com/cjwit/gettingout",
 		link: 'https://cjwit-gettingout.herokuapp.com'
 	},
 	{
 		name: 'Vote!',
 		img: 'images/vote.png',
 		description: "Voting app built in React/Flux using D3.js to represent user created polls.",
-		github: "https://github.com/cjwit/vote",
+		repo: "https://github.com/cjwit/vote",
 		link: 'https://cjwit-vote.herokuapp.com'
 	},
 	{
 		name: 'Markdown Previewer',
 		img: 'images/markdown.png',
 		description: "A simple online text editor that displays a preview of Github's Markdown format.",
-		github: 'https://github.com/cjwit/markdown/tree/gh-pages',
+		repo: 'https://github.com/cjwit/markdown/tree/gh-pages',
 		link: 'http://cjwit.github.io/markdown/'
 	},
 	{
 		name: 'Recipe Box',
 		img: 'images/recipes.png',
 		description: "Recipe box application designed in React.js that uses the browser's local storage to persist saved entries.",
-		github: "https://github.com/cjwit/recipes/tree/gh-pages",
+		repo: "https://github.com/cjwit/recipes/tree/gh-pages",
 		link: 'http://cjwit.github.io/recipes'
 	},
 	{
 		name: 'Dungeon Fungeon',
 		img: 'images/dungeon.png',
 		description: "A dungeon-type game built in React.js.",
-		github: "https://github.com/cjwit/fungeon/tree/gh-pages",
+		repo: "https://github.com/cjwit/fungeon/tree/gh-pages",
 		link: 'http://cjwit.github.io/fungeon'
 	},
 	{
 		name: 'Game of Life',
 		img: 'images/life.png',
 		description: "React.js-based simulation that mirrors the functionality of Conway's Game of Life.",
-		github: "https://github.com/cjwit/life/tree/gh-pages",
+		repo: "https://github.com/cjwit/life/tree/gh-pages",
 		link: 'http://cjwit.github.io/life'
 	}
 ]
@@ -174,27 +174,27 @@ var other_websites = [
 		name: 'Online Course Documentation',
 		img: 'images/docs.png',
 		description: "Documentation for teaching online courses. Using Ajax, the page accesses an editable Markdown file and renders it in HTML and generates a responsive menu from the content headings for easy navigation.",
-		github: "https://github.com/cjwit/onlinedocs/",
+		repo: "https://github.com/cjwit/onlinedocs/",
 		link: 'https://cjwit.github.io/onlinedocs/'
 	},
 	{
 		name: 'Responsive E-Commerce Layout',
 		img: 'images/ecommerce.png',
 		description: "Responsive template format for an e-commerce website that uses jQuery and Bootstrap to render content from a mock API call.",
-		github: "https://github.com/cjwit/coffeelayout",
+		repo: "https://github.com/cjwit/coffeelayout",
 		link: 'http://cjwit.github.io/coffeelayout'
 	},
 	{
 		name: 'Teaching portfolio',
 		img: 'images/portfolio.png',
 		description: "Dynamically presents multimedia teaching portfolio content using jQuery, Bootstrap, and Colorbox.",
-		github: "https://github.com/cjwit/markdown/tree/gh-pages",
+		repo: "https://github.com/cjwit/markdown/tree/gh-pages",
 		link: 'http://cjwit.github.io/jw'
 	},
 	{
 		name: 'Portfolio and Home Page',
 		description: "Uses Bootstrap and SASS to display portfolio content and other information on the author's research, musical activities, and current web development projects.",
-		github: "https://github.com/cjwit/cjwit.github.io",
+		repo: "https://github.com/cjwit/cjwit.github.io",
 		link: '#musicology'
 	},
 	{
@@ -206,28 +206,28 @@ var other_websites = [
 		name: 'Random Quote Generator',
 		img: 'images/quotes.png',
 		description: "API-driven application generates a new quote and allows for sharing via Twitter",
-		github: "https://github.com/cjwit/quotes/tree/gh-pages",
+		repo: "https://github.com/cjwit/quotes/tree/gh-pages",
 		link: 'http://cjwit.github.io/quotes'
 	},
 	{
 		name: 'Wikipedia Lookup',
 		img: 'images/wiki.png',
 		description: "Small app that uses jQuery and API calls to Wikipedia to generate and format links for articles based on a search request.",
-		github: "https://github.com/cjwit/wiki/tree/gh-pages",
+		repo: "https://github.com/cjwit/wiki/tree/gh-pages",
 		link: 'http://cjwit.github.io/wiki'
 	},
 	{
 		name: 'Calculamatator',
 		img: 'images/calc.png',
 		description: "Javascript-based online calculator application.",
-		github: "https://github.com/cjwit/calc/tree/gh-pages",
+		repo: "https://github.com/cjwit/calc/tree/gh-pages",
 		link: 'http://cjwit.github.io/calc'
 	},
 	{
 		name: 'Current Weather',
 		img: 'images/weather.png',
 		description: "Weather application that uses geographic coordinates from the browser to find the current weather and near-term forecast for your local area.",
-		github: "https://github.com/cjwit/weather/tree/gh-pages",
+		repo: "https://github.com/cjwit/weather/tree/gh-pages",
 		link: 'http://cjwit.github.io/weather'
 	}
 ]
@@ -246,8 +246,8 @@ var renderContent = function (category) {
 		html += "</p><p><a href='";
 		html += item.link;
 		html += "' class='btn btn-default' target='_blank' role='button'>Live page</a>"
-		if (item.github) {
-			html += " <a href='" + item.github + "' class='btn btn-default' target='_blank' role='button'>View on GitHub</a>"
+		if (item.repo) {
+			html += " <a href='" + item.repo + "' class='btn btn-default' target='_blank' role='button'>View code</a>"
 		}
 		html += "</p></div></div>"
 		content += html
